09e65604bb784eb3ad4b9c0bcb10f2935cb996c3
Justin Hofer <justin.hofer88@gmail.com>
2020-04-15 21:08:40 -0500

    Camera controls
    Spawn Turrets and added camera controls

---
+++ Assets/BuildManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogError("More than one build manager in scene");
+            return;
+        }
+
+        instance = this;
+    }
+
+    public GameObject standardTurretPrefab;
+
+    private void Start()
+    {
+        turretToBuild = standardTurretPrefab;
+    }
+
+    private GameObject turretToBuild;
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+}
